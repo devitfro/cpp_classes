9432285cc19c8db4805e0802d2441fab4aef6b08
devitfro <potapovaaa.alina@gmail.com>
2024-09-01 21:18:33 +0300

    lesson1 hw

---
+++ lesson1/ConsoleApplication1/hw1/hw1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//task 1
+	//cout << "\"To be\n\tor not\n\t\tto be...\"\n\t\t\t\/ Shakespeare \/";
+
+	//task 2
+	//cout << "I love C++!\n\tI love C++!\n\t\tI love C++!\n\t\t\tI love C++!\n\t\t\t\tI love C++!\n";
+
+	//task 3
+	/*cout << "   ***            ***         ***    ****     ***    ***        ***\n";
+	cout << " ***  ***       ***  ***      ***   * ***     ***    ***      ***  ***\n";
+	cout << "**********     ***    ***     ***  ** ***     **********     **********\n";
+	cout << "***    ***     ***    ***     *** **  ***     ***    ***     ***    ***\n";
+	cout << "***    ***     ***    ***     *****   ***     ***    ***     ***    ***\n";*/
+
+	//task 4
+	/*cout << "Vladimir && Natali\n";
+	cout << "\t  *\n";
+	cout << "\t  *\n";
+	cout << "\t  *\n";
+	cout << "\t * *\n";
+	cout << "\t*   *\n";
+	cout << "\n";
+	cout << "\tAlina\n";*/
+
+	//task 5
+	//cout << "                * * * * *\n";
+	//cout << "               *\n";
+	//cout << "              *\n";
+	//cout << "            ***\n";
+	//cout << "            ***\n";
+	//cout << "      *********\n";
+	//cout << "     ***********\n";
+	//cout << "    *************\n";
+	//cout << "   ***************\n";
+	//cout << "   ***************\n";
+	//cout << "   ***###*********\n";
+	//cout << "   ***###*****_***\n";
+	//cout << "   ***###****|*|**\n";
+	//cout << "   **********|*|**\n";
+	//cout << "$$$**********|*|**$$$$$$$$$$$$$$$$$\n";
+
+	//task 6
+	//cout << "Name: Alina\n";
+	//cout << "Lastname: Potapova\n";
+	//cout << "Programming language: \'C++\'\n";
+	//cout << "Experiense: 3 years\n";
+	//cout << "English: Upper-intermediate\n";
+
+}
---
+++ lesson2/lesson2/lesson2.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    cout << boolalpha;
+    bool is_cpp_fun = 5;
+    count << is_cpp_fun;
+
+}
